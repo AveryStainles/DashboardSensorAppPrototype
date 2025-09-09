bf3f9d15817a4f11461db9caad44e21e758808bd
Vallee-Vezina2001 <86313959+AveryStainles@users.noreply.github.com>
2025-09-09 09:30:43 -0400

    Delete DashboardSensorAppPrototype directory

--- DashboardSensorAppPrototype/DashboardSensorAppPrototype/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace DashboardSensorAppPrototype.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
