a26cef3edbcc49039c4cc8d5ee25057fb5472562
Vallee-Vezina2001 <86313959+AveryStainles@users.noreply.github.com>
2025-10-17 22:00:19 -0400

    Add files via upload

---
+++ MqttDemo/Helpers/FileReadWriteHelper.cs
@@ -0,0 +1,20 @@
+﻿namespace MqttDemo.Helpers
+{
+    public class FileReadWriteHelper
+    {
+        public async static Task WriteToReportAsync(string content)
+        {
+            var fileName = "Reports.txt";
+            var directory = GoUpDirectoryLevelHelper(Environment.CurrentDirectory, 2);
+            var path = Path.Combine(directory, fileName);
+
+            // Append to file
+            using (StreamWriter outputFile = new StreamWriter(path, true))
+                await outputFile.WriteAsync($"{DateTime.Now}\t|\t{content}{Environment.NewLine}");
+        }
+
+        public static string GoUpDirectoryLevelHelper(string path, int depth) => (depth > 0)
+            ? GoUpDirectoryLevelHelper(path.Substring(0, path.LastIndexOf('\\')), depth - 1)
+            : path.Substring(0, path.LastIndexOf('\\'));
+    }
+}
---
+++ MqttDemo/Models/SubscribedTopic.cs
@@ -0,0 +1,21 @@
+﻿using MQTTnet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MqttDemo.Models
+{
+    public class SubscribedTopic
+    {
+        // Constructors
+        public SubscribedTopic() { Topic = string.Empty; }
+        public SubscribedTopic(string topic) { Topic = topic; }
+
+        // Properties
+        public string Topic { get; set; }
+        public string LastMessage { get; set; } = string.Empty;
+        public List<Func<MqttApplicationMessageReceivedEventArgs, Task>> EventListeners { get; set; } = new();
+    }
+}
