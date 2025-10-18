232260fe41c20cdbfb294dc6f31442e1465ac7de
Vallee-Vezina2001 <86313959+AveryStainles@users.noreply.github.com>
2025-10-17 21:56:42 -0400

    Delete DashboardSensor directory

--- DashboardSensor/DashboardSensor/Models/HomeViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DashboardSensor.Models
-{
-    public class HomeViewModel
-    {
-        public List<string> SubscriptionResponse { get; set; } = new();
-        public bool IsServerRunning { get; set; }
-    }
-}
